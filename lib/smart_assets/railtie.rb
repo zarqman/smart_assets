e5d649f31cb46045b7b23d07d751566b9e6324a9
thomas morgan <tm@iprog.com>
2024-11-06 12:23:44 -0700

    drop unused legacy code

--- lib/smart_assets/railtie.rb
+++ lib/smart_assets/railtie.rb
@@ -11,13 +11,7 @@ module SmartAssets
         prefix = app.config.smart_assets.prefix || app.config.assets.prefix || '/assets'
         cc = app.config.smart_assets.cache_control
 
-        if app.config.respond_to?(:public_file_server)
-          app.config.public_file_server.enabled = true  # >= 5.0
-        elsif app.config.respond_to?(:serve_static_files)
-          app.config.serve_static_files = true          #  = 4.2
-        else
-          app.config.serve_static_assets = true         # <= 4.1
-        end
+        app.config.public_file_server.enabled = true
 
         app.middleware.insert_after(::Rack::Sendfile, SmartAssets::Rack, prefix, cc)
       end
